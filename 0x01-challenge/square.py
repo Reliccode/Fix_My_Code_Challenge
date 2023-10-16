e1517c632535f390666d5c949ca33561782fc046
Reliccode <bryanmaritim@gmail.com>
2023-10-16 21:13:29 +0300

    [Fix] Adding docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,51 @@
 #!/usr/bin/python3
-"""Module for square class"""
+"""Module for class square"""
+
 
 class Square():
-    """ Class representing square"""
+    """A class representing a square."""
 
-    width = 0
-    height = 0
+    def __init__(self, side_length):
+        """
+        Initialize a Square object with the given side length.
 
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        Args:
+            side_length (int): The side length of the square.
+        """
+        self.width = side_length
+        self.height = side_length
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        """Area of perimeter"""
-        return (self.width * 2) + (self.height * 2)
+    def perimiter_of_my_square(self):
+        """
+        Calculate the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
+        return self.width * 4
 
     def __str__(self):
-        """Self initialize"""
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string in the format 'width/height'.
+        """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=12)
+    s = Square(side_length=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimiter_of_my_square())
