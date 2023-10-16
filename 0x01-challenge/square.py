cdd336778c204f6d1bc02aee3f4b3616002aede0
Reliccode <bryanmaritim@gmail.com>
2023-10-16 19:47:25 +0300

    Revert "Refactor Square class:"
    This reverts commit a5329cecffe2200dbf528dc19b823577f8ad45e5.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 
-class Square():
+class square():
+    
+    width = 0
+    height = 0
 
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Calculate the area of the square """
+        """ Area of the square """
         return self.width * self.width
 
-    def perimeter_of_my_square(self):
-        """Calculate the perimeter of the square"""
-        return 4 * self.width
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = square(width=12, height=9)
     print(s)
-    print("Area:", s.area_of_my_square())
-    print("Perimeter:", s.perimeter_of_my_square())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
