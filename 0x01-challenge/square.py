296647eaed0f32211b7c0d1e3e94b6c7de9ba893
Reliccode <bryanmaritim@gmail.com>
2023-10-16 20:54:48 +0300

    [Fix] Adding docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
 
-class Square:
+class Square():
+    """A class representing a square."""
 
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+    def __init__(self, side_length):
+        """
+        Initialize a Square object with the given side length.
+
+        Args:
+            side_length (int): The side length of the square.
+        """
+        self.width = side_length
+        self.height = side_length
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.height
+        """
+        Calculate the area of the square.
 
-    def perimeter_of_my_square(self):
-        return (self.width * 2) + (self.height * 2)
+        Returns:
+            int: The area of the square.
+        """
+        return self.width * self.width
+
+    def perimiter_of_my_square(self):
+        """
+        Calculate the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
+        return self.width * 4
 
     def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string in the format 'width/height'.
+        """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main":
-    s = Square(width=12, height=9)
+
+if __name__ == "__main__":
+
+    s = Square(side_length=12)
     print(s)
-    print("Area of my square:", s.area_of_my_square())
-    print("Perimeter of my square:", s.perimeter_of_my_square())
+    print(s.area_of_my_square())
+    print(s.perimiter_of_my_square())
