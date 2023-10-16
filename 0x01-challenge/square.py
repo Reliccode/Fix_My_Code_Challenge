f5f28bac1dacb555c5793915719e5bc2df24f2d2
Reliccode <bryanmaritim@gmail.com>
2023-10-16 19:52:50 +0300

    Refactor Square class: - Renamed class name to 'Square'. - Renamed method 'PermiterOfMySquare' to 'perimeter_of_my_square'.- Initialized attributes within the constructor. - Updated docstrings for clarity. - Corrected area calculation in 'area_of_my_square' method.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square():
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -14,15 +13,17 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimiter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
